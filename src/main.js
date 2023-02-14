f3f6298ad5975b8795ebc7268987c855f31df897
Nathan de assis Crevelin <neitass.music@gmail.com>
2023-02-14 16:52:12 -0300

    termina funçao fetchProductsList

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { searchCep } from './helpers/cepFunctions';
-import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);

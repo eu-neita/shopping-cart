2cf821f27a4d3622c4f171c84bf81aa2dc066521
Nathan de assis Crevelin <neitass.music@gmail.com>
2023-02-14 18:08:27 -0300

    termina funçao fetchProductsList

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -3,12 +3,8 @@ export const fetchProduct = () => {
 };
 
 export const fetchProductsList = async (query) => {
-  try {
-    if (query === '') throw new TypeError('Termo de busca não informado');
-    const result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
-    const data = await result.json();
-    return data.results;
-  } catch (error) {
-    return error.message;
-  }
+  if (!query) throw new Error('Termo de busca não informado');
+  const result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const data = await result.json();
+  return data.results;
 };

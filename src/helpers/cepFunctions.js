09508aa9571d93aa5969e5abedfea35fc6f13812
Nathan de assis Crevelin <neitass.music@gmail.com>
2023-02-15 16:58:25 -0300

    termina requisito 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,10 +1,10 @@
 export const getAddress = async (cep) => {
   if (!cep) throw new Error('CEP não informado');
-  const promise2 = fetch(`https://cep.awesomeapi.com.br/json/${cep}`);
-  const promise3 = fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`);
-  const promises = [promise2, promise3];
-  const promise = await Promise.any(promises);
-  const dataCep = await promise.json();
+  const promise1 = fetch(`https://cep.awesomeapi.com.br/json/${cep}`);
+  const promise2 = fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`);
+  const promises = [promise1, promise2];
+  const result = await Promise.any(promises);
+  const dataCep = await result.json();
   return dataCep;
 };
 
@@ -12,13 +12,16 @@ const cepDisplay = document.querySelector('.cart__address');
 
 export const searchCep = async () => {
   try {
-    const cep = document.querySelector('.cep-input').value;
-    const data = await getAddress(cep);
-    if (data.city === undefined) throw new Error('CEP não encontrado');
+    const cepValue = document.querySelector('.cep-input').value;
+    const data = await getAddress(cepValue);
+    if (data === 'URL não mapeada') throw new Error('CEP não encontrado');
     cepDisplay
-      .innerHTML = `${data.address_name} - ${data
+      .innerHTML = `${data.address} - ${data
         .district} - ${data.city} - ${data.state}`;
   } catch (error) {
     cepDisplay.innerHTML = error.message;
+    if (cepDisplay.innerHTML === 'URL não mapeada') {
+      cepDisplay.innerHTML = 'CEP não encontrado';
+    }
   }
 };
